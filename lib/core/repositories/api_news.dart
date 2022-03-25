5402d44d096ad13ccd06b5852030656f1ac91fac
Daniel Brandao <daniel.brandao@residencia.dev>
2022-03-25 20:25:40 -0300

    criado ArticlesModel, SourceModel e ApiNews

---
+++ lib/core/repositories/api_news.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:app_noticias/core/models/article_model.dart';
+import 'package:http/http.dart' as http;
+
+class ApiNews {
+  final _path =
+      'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=23c30098b82a42828f2036b9fe9e0ec3';
+
+  Future<List<ArticleModel>> getNews() async {
+    final response = await http.get(Uri.parse(_path));
+
+    try {
+      if (response.statusCode == 200) {
+        final decodeJson = jsonDecode(response.body);
+
+        final articlesList = decodeJson['articles'] as List;
+        return articlesList
+            .map((article) => ArticleModel.fromJson(article))
+            .toList();
+      }
+      return [];
+    } catch (error) {
+      throw Exception(error);
+    }
+  }
+}
